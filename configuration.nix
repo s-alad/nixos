a3f22265cdc89efff4285208a9b6f96f5a1c7632
s-alad <n7.saad@gmail.com>
2025-12-25 19:09:42 -0500

    current

---
+++ configuration.nix
@@ -0,0 +1,332 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+
+  ##### NIX
+  # nix.gc = {
+  #   automatic = true;
+  #   dates = "weekly";
+  #   options = "--delete-older-than 14d";
+  # };
+
+
+  ##### BOOTLOADER
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # --- systemd initrd for TPM2 auto-unlock
+  boot.initrd.systemd.enable = true;
+  # --- LUKS devices (TPM2 enrolled)
+  # - $ sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7 /dev/nvme1n1p2
+  # - $ sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7 /dev/nvme1n1p3
+  boot.initrd.luks.devices = {
+    "luks-d96ca84d-a4ef-4faf-944d-892d3bf7e910" = {
+      device = "/dev/disk/by-uuid/d96ca84d-a4ef-4faf-944d-892d3bf7e910";
+    };
+    "luks-fcd35927-da32-4090-9abc-51eb75a4a6d5" = {
+      device = "/dev/disk/by-uuid/fcd35927-da32-4090-9abc-51eb75a4a6d5";
+    };
+  };
+  # --- plymouth startup animation
+  # boot.plymouth = {
+  #  enable = true;
+  #  theme = "bgrt";
+  # };
+  # --- silent boot
+  # boot.consoleLogLevel = 3;
+  # boot.initrd.verbose = false;
+  # boot.kernelParams = [
+  #  "quiet"
+  # "splash"
+  #  "boot.shell_on_fail"
+  #  "udev.log_priority=3"
+  #  "rd.systemd.show_status=auto"
+  # ];
+  # hide the OS choice for bootloaders unless any key pressed
+  # boot.loader.timeout = 0;
+
+  
+
+  ##### NETWORKING 
+  # --- hostname
+  networking.hostName = "salad";
+  # --- enable networking
+  networking.networkmanager.enable = true;
+  # --- wpa_supplicant
+  # networking.wireless.enable = true;
+  # --- network proxy
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # --- firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # networking.firewall.enable = false;
+
+
+  ##### TIME
+  time.timeZone = "America/New_York";
+
+
+  ##### INTL
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+
+  ##### HARDWARE
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [ 
+      intel-media-driver # video acceleration
+      # intel-compute-runtime # opencl
+    ];
+  };
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;   # tear-free rendering on X11
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      # Optimus PRIME Option A: Offload Mode
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      # Optimus PRIME Option B: Sync Mode
+      # sync.enable = true;        # smoother performance ; less battery
+
+      # BUS ID
+      intelBusId = "PCI:0:2:0";  # from lspci: 00:02.0
+      nvidiaBusId = "PCI:1:0:0"; # from lspci: 01:00.0
+    };
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+  };
+  hardware.sensor.iio.enable = true;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.enableRedistributableFirmware = true;
+  # --- explicit bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+
+  ##### ENV
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
+
+  ##### SERVICES
+  ### CINNAMON
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+  ### SOUND with pipewire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  # --- flatpak
+  services.flatpak.enable = true;
+  # --- 
+  services.power-profiles-daemon.enable = true;
+  # --- system wide power management
+  powerManagement.enable = true;
+  # --- X11 windowing system
+  services.xserver.enable = true;
+  # --- NVIDIA + intel hybrid graphics ; ThinkPad P1 Gen 8
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+  # --- firmware updates
+  services.fwupd.enable = true;
+  # --- intel thermal management
+  services.thermald.enable = true;
+  # --- 
+  services.fstrim.enable = true;
+  # --- keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  # --- enable CUPS to print documents
+  services.printing.enable = true;
+  # --- touchpad support (enabled default in most desktopManager)
+  # services.xserver.libinput.enable = true;
+  # --- OpenSSH daemon ; inbound
+  # services.openssh.enable = true;
+  # --- mouse support in tty
+  services.gpm.enable = true;
+  # --- fingerprint reader (Synaptics 06cb:00f9)
+  # services.fprintd.enable = true;
+  # --- allow both fingerprint AND password authentication
+  # security.pam.services.login.fprintAuth = false;
+  # --- enable fingerprint for specific services
+  # security.pam.services.lightdm.fprintAuth = true;
+  # security.pam.services.sudo.fprintAuth = true;
+  # security.pam.services.polkit-1.fprintAuth = true;
+
+
+  ### XDG
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
+
+
+  ##### USERS
+  users.users.salad = {
+    isNormalUser = true;
+    description = "salad";
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" "kvm" "docker" ];
+    packages = with pkgs; [
+      thunderbird
+      bottles
+      discord
+      slack
+      claude-code
+      code-cursor
+      brave
+    ];
+  };
+
+
+  ##### PROGRAMS
+  # --- firefox
+  programs.firefox.enable = true;
+  # --- steam
+  # programs.steam = {
+  #  enable = true;
+  #  remotePlay.openFirewall = true;  # Open ports in firewall for Steam Remote Play
+  #  dedicatedServer.openFirewall = true;  # Open ports for Source Dedicated Server
+  #  localNetworkGameTransfers.openFirewall = true;  # Open ports for Steam Local Network Game Transfers
+  #  gamescopeSession.enable = true;  # Enable GameScope for better gaming performance
+  # };
+  # --- gamemode (automatic performance optimizations when gaming)
+  # programs.gamemode.enable = true;
+  # --- prebuilt linux binaries
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    fuse2
+    stdenv.cc.cc
+    zlib
+    openssl
+    glib
+    gtk3
+    xorg.libX11
+    xorg.libxcb
+    mesa
+    libdrm
+  ];
+
+
+  ##### PACKAGES
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    efibootmgr
+    pciutils
+    usbutils
+    btop
+    htop
+    git
+    mesa-demos
+    vulkan-tools
+    nvtopPackages.full
+    vscode
+    nh
+    helix
+    libva-utils
+    gnome-software
+    signal-desktop
+    fuse2
+    neofetch
+    file
+    nodejs
+    yarn
+    go
+  ];
+
+
+  ##### DOCKER
+  virtualisation.docker = {
+    enable = true;
+  };
+
+
+  ##### NIXPKGS
+  # --- allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+
+  ##### SETTINGS
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [ "@wheel" ];
+      auto-optimise-store = true;
+    };
+  };
+
+
+  ##### FONTS
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    roboto
+    nerd-fonts.iosevka
+    comic-mono
+    aileron
+    atkinson-hyperlegible
+    cantarell-fonts
+    adwaita-fonts
+  ];
+
+
+  ##### PROGRAMS
+  # --- network diagnostic tool
+  # programs.mtr.enable = true;
+  # --- GPG/SSH agent
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  # --- NH https://github.com/nix-community/nh
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+  };
+
+
+  ##### SYSTEM
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
