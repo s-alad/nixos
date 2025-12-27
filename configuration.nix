3fb25904557c3e18e16740ce978eafca3e11b09f
s-alad <n7.saad@gmail.com>
2025-12-27 15:39:31 -0500

    back to lightdm

--- configuration.nix
+++ configuration.nix
@@ -150,8 +150,7 @@ in
 
   ##### SERVICES
   ### CINNAMON
-  #services.xserver.displayManager.lightdm.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.cinnamon.enable = true;
   ### GNOME
   # services.xserver.displayManager.gdm.enable = true;
