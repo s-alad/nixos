41659662c534a8329d884c2a71ec12fe8c3f9764
s-alad <n7.saad@gmail.com>
2025-12-29 01:20:48 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NIXOS FLAKE";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ...}@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
