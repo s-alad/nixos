b5a5e34fd263e905ba0f64099ad1bd3ec58a9f88
s-alad <n7.saad@gmail.com>
2025-12-29 01:46:23 -0500

    destabalize flake but pin cinnamon

--- flake.nix
+++ flake.nix
@@ -2,20 +2,28 @@
   description = "NIXOS FLAKE";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # home-manager = {
-    #   url = "github:nix-community/home-manager/release-25.11";
+    #   url = "github:nix-community/home-manager";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, ...}@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-stable, ...}@inputs: {
     nixosConfigurations.salad = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
         # inputs.home-manager.nixosModules.default
+
+        # pin cinnamon to stable
+        ({ pkgs, ... }: {
+          nixpkgs.config.packageOverrides = pkgs: {
+            cinnamon = nixpkgs-stable.legacyPackages.${pkgs.system}.cinnamon;
+          };
+        })
       ];
     };
   };
