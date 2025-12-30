bbabd95471e75e057cebecdcb0612cce25f406c2
s-alad <n7.saad@gmail.com>
2025-12-30 17:16:29 -0500

    steam click

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -5,8 +5,19 @@
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+
+    # Fix for input/click issues in Steam games on X11
+    # Forces SDL2 to use X11 video backend properly
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 
   # --- gamemode (automatic performance optimizations when gaming)
   programs.gamemode.enable = true;
+
+  # Environment variables to fix Steam input handling on X11
+  environment.sessionVariables = {
+    SDL_VIDEODRIVER = "x11";
+  };
 }
